3f441ceb2a7195027a09eef3bd92ac20ac276536
Ekaterina Karpuk <kate.ff.usa@gmail.com>
2025-10-25 12:49:31 +0200

    Update webdriver manager with threadlocal

--- no-spring-testng/src/test/java/demoqa/core/page/BasePage.java
+++ no-spring-testng/src/test/java/demoqa/core/page/BasePage.java
@@ -5,7 +5,7 @@ import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 
-public class BasePage {
+public abstract class BasePage {
 
     protected WebDriver driver;
     public static Logger logger = LogManager.getLogger();
