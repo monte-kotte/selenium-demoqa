f72c64e6f52263a4b426d8239d3ef4d6b5596ae1
Ekaterina Karpuk <kate.ff.usa@gmail.com>
2025-10-25 12:30:18 +0200

    Extract constants and before each

--- no-spring-testng/src/test/java/demoqa/test/ElementsTest.java
+++ no-spring-testng/src/test/java/demoqa/test/ElementsTest.java
@@ -2,22 +2,24 @@ package demoqa.test;
 
 import demoqa.core.model.User;
 import org.assertj.core.api.Assertions;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-import static demoqa.test.data.Constants.TEST_USER;
-import static demoqa.test.data.Constants.TEST_USER_INCORRECT_EMAIL;
+import static demoqa.test.data.Constants.*;
 import static demoqa.test.data.TestDataFactory.*;
 
 public class ElementsTest extends TestBase {
 
-    public static final String ELEMENTS_URL = "https://demoqa.com/elements";
+    @BeforeMethod
+    public void setUpElementsTest() {
+        elementsPage.navigateTo(ELEMENTS_URL);
+        elementsPage.openTextBoxPage();
+    }
 
     @Test
     void textBoxSuccessTest() throws Exception {
         User testUser = fromFile(TEST_USER, User.class);
 
-        elementsPage.navigateTo(ELEMENTS_URL);
-        elementsPage.openTextBoxPage();
         elementsService.fillTextBoxForm(testUser);
         textBoxPage.submitForm();
         User actualUser = elementsService.getTextBoxOutputUser();
@@ -31,8 +33,6 @@ public class ElementsTest extends TestBase {
     void incorrectEmailFormatTest() throws Exception {
         User testUserWithIncorrectEmail = fromFile(TEST_USER_INCORRECT_EMAIL, User.class);
 
-        elementsPage.navigateTo(ELEMENTS_URL);
-        elementsPage.openTextBoxPage();
         elementsService.fillTextBoxForm(testUserWithIncorrectEmail);
         textBoxPage.submitForm();
 
--- no-spring-testng/src/test/java/demoqa/test/data/Constants.java
+++ no-spring-testng/src/test/java/demoqa/test/data/Constants.java
@@ -2,7 +2,15 @@ package demoqa.test.data;
 
 public class Constants {
 
+    // Test data files
     public static final String TEST_USER = "/template-user.json";
     public static final String TEST_USER_INCORRECT_EMAIL = "/template-user-incorrect-email.json";
+
+    // URLs
+    public static final String BASE_URL = "https://demoqa.com/";
+    public static final String ELEMENTS_URL = "https://demoqa.com/elements";
+
+    // Page titles
+    public static final String MAIN_PAGE_TITLE = "DEMOQA";
 
 }
